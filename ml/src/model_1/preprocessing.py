00ecbe1d0e9c4903dfff72cdd8eb33bbf0978589
kishore-sv <kishorbunni7@gmail.com>
2026-09-11 10:11:29 +0530

    models:all models updated with university data : https://github.com/kishore-sv/student-results-dataset

--- ml/src/model_1/preprocessing.py
+++
@@ -1,5 +0,0 @@
-"""Build feature DataFrame for Model 1."""
-
-from src.common.validation import to_feature_dataframe
-
-__all__ = ["to_feature_dataframe"]
--- ml/src/model_2/preprocessing.py
+++
@@ -1,5 +0,0 @@
-"""Build feature DataFrame for Model 2."""
-
-from src.common.validation import to_feature_dataframe
-
-__all__ = ["to_feature_dataframe"]
